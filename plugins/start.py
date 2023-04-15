e386d132cb219f0f3d8d08c71f82d8e4e2bd8651
yildizcavres <129795323+yildizcavres@users.noreply.github.com>
2023-04-15 18:50:03 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,9 +7,9 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 @Client.on_message(filters.command('start'))
 async def start(bot, message):
     name = message.from_user.first_name
-    url = "https://t.me/Cengonuzz"
+    url = "https://t.me/Kelebekfedkurucuu"
     text = f"Merhaba {name}\n\nBu bot sayesinde yașadığın yerdeki iftar ve sahur saatlerini ve ne kadar kaldığını öğrenebilirsin 😁.\n\n`/sahur Ankara mamak`\n`/iftar Ankara mamak`\n\nHayırlı Ramazanlar [:Destek]({url})"
-    photo = "https://telegra.ph/Sohbet-vefa-03-28"
+    photo = ""
     await bot.send_photo(
         chat_id=message.chat.id,
         photo=photo, 
